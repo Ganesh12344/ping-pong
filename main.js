5f5a1354ffd38e66896e06566f77aeb9637dc983
Ganesh12344 <82883868+Ganesh12344@users.noreply.github.com>
2022-06-08 20:20:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+  }
+  
+  function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
